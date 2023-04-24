2d233112d3531fc42224548514188d46e7afba78
huanmeng <huanmengqwq@gmail.com>
2023-04-24 23:10:50 +0800

    0.2

---
+++ src/main/java/com/restaurantorder/ordertest/servlet/backstageIndexServlet.java
@@ -0,0 +1,50 @@
+package com.restaurantorder.ordertest.servlet;
+
+import com.restaurantorder.ordertest.service.AdminService;
+import com.restaurantorder.ordertest.service.impl.AdminServiceImpl;
+import com.restaurantorder.ordertest.util.ThymeleafUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.thymeleaf.context.Context;
+
+import java.io.IOException;
+
+@WebServlet("/adminIndex")
+public class backstageIndexServlet extends HttpServlet {
+    AdminService adminService;
+
+    @Override
+    public void init() throws ServletException {
+        adminService = new AdminServiceImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Context context = new Context();
+
+        HttpSession session = req.getSession();
+        String name = (String) session.getAttribute("name");
+        context.setVariable("name", name);
+
+        ThymeleafUtil.getEngine().process("backstage-index.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String note = req.getParameter("note");
+
+        if (note != null) {
+            adminService.ChangeAnnouncements(note);
+
+            session.setAttribute("changeMsg", "修改成功！");
+        } else {
+            session.setAttribute("changeMsg", "修改失败！");
+        }
+        resp.sendRedirect("adminIndex");
+    }
+}
